8eb3ae7f435860dded0bb4dded4e5fb106d64e4a
sreedhar <sreedhar70755@gmail.com>
2023-08-25 12:17:41 +0530

    tf

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install ansible -y  &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/sreedharm07/roboshop-ansible.git main.yml -e component=rabbitmq  &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/sreedharm07/roboshop-ansible.git  main.yml -e component=rabbitmq  &>>/opt/userdata.log
